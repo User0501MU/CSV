129f1713968a689fa186aca6380424c10857d256
User0501MU <m-utsunomiya@fores.jp>
2024-08-23 15:52:15 +0900

    「main」関数が2つ以上あるとどっちを実行するかって聞かれるので自動生成ファイルは今回不要

--- src/main/java/com/example/CsvSpringApplication.java
+++ src/main/java/com/example/CsvSpringApplication.java
@@ -1,13 +1,16 @@
-package com.example;
+//package com.example;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class CsvSpringApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(CsvSpringApplication.class, args);
+//	}
+//
+//}
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CsvSpringApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CsvSpringApplication.class, args);
-	}
-
-}
+//CSVファイルとCsvSPringApplication.javaで「main」関数が2つ以上あるとどっちを実行するかって聞かれる
+//main関数を一番最初に実行しようとするので、一つのプロジェクトにつき必ず一つという状態にしておかないと今回みたいなことが起きます。
